f8305d389a05ba16c13e638c4b778754cbb72c89
Brian Wright <briankw23@gmail.com>
2018-06-27 17:54:12 -0500

    listing printing to dom

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -3,9 +3,18 @@ import './Listings.css';
 
 class Listings extends React.Component {
   render () {
+    const {listings} = this.props;
+    const listingsItemsComponents = listings.map((listing) => {
+      return (
+        <li key={listing.id} index={listing.id}>{listing.price}</li>
+      );
+    });
     return (
       <div className='Listings'>
         <h2>Listings</h2>
+        <ul>
+          {listingsItemsComponents}
+        </ul>
       </div>
     );
   }
