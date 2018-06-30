c24726639252d6caf4428a6aed46e84e9e6c6ab7
Brian Wright <briankw23@gmail.com>
2018-06-30 11:21:01 -0500

    created listing items components and props

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {listingShape} from '../../propz/listingProp';
 import './Listings.css';
+import ListingItem from '../ListingItem/ListingItem';
 
 class Listings extends React.Component {
   static propTypes = {
@@ -11,7 +12,11 @@ class Listings extends React.Component {
     const {listings} = this.props;
     const listingsItemsComponents = listings.map((listing) => {
       return (
-        <li key={listing.id} index={listing.id}>{listing.price}</li>
+        // <li key={listing.id} index={listing.id}>{listing.price}</li>
+        <ListingItem
+          key={listing.id}
+          listing={listing}
+        />
       );
     });
     return (
