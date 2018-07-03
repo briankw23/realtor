d03af074ca936d0f8b53cd8567bc6cccc1b8f8ff
Brian Wright <briankw23@gmail.com>
2018-07-02 19:04:46 -0500

    added single building

--- src/app/App.js
+++ src/app/App.js
@@ -30,6 +30,8 @@ class App extends Component {
       });
   }
   render () {
+    const {selectedListingId, listings} = this.state;
+    const selectedListing = listings.find(listing => listing.id === selectedListingId);
     return (
       <div className='App'>
         <div className="col-sm-6">
@@ -39,7 +41,7 @@ class App extends Component {
           />
         </div>
         <div className="col-sm-6">
-          <Building/>
+          <Building listing={selectedListing}/>
         </div>
         <div className="col-sm-12">
           <ListingForm/>
